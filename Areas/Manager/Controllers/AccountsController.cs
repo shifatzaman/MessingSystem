71161e252bd6c91ceea6263fd77eec23fe7c6553
Shifat Zaman <shifatzaman034@gmail.com>
2021-10-22 23:39:32 +0600

    Make billing page design update

---
+++ Areas/Manager/Controllers/AccountsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessingSystem.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    public class AccountsController : Controller
+    {
+        private readonly ILogger<AccountsController> _logger;
+
+        public AccountsController(ILogger<AccountsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult ExtraMessing()
+        {
+            return View();
+        }
+
+        public IActionResult MonthlyBill()
+        {
+            return View();
+        }
+
+        public IActionResult CafeteriaBill()
+        {
+            return View();
+        }
+
+    }
+}
